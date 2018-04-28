3a92cd330ccd5a29105563c633349b75cbaac0dc
Kamal Kumar <kamalverma1593@gmail.com>
2018-04-28 17:04:11 +0530

    my project setup completed successfully

---
+++ shoppingbackend/src/main/java/com/kksoft/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.kksoft.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
